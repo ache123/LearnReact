f292c73293fe004a75c8994acb8d57df6572e7fc
ache123 <631346398@qq.com>
2021-02-09 18:06:26 +0800

    直接使用react-redux库

---
+++ 14_learn_react_redux/src/pages/Home3.js
@@ -0,0 +1,36 @@
+import React from 'react'
+// import connect from '../utils/connect'
+import { connect } from 'react-redux'
+
+
+import {
+  incAction,
+  addAction
+} from '../store/actionCreators'
+
+function Home(props) {
+  return (
+    <div>
+      <hr />
+      <h1>Home</h1>
+      <h2>当前计数：{props.counter}</h2>
+      <button onClick={e => props.increment()}>+1</button>
+      <button onClick={e => props.addNumber(5)}>+5</button>
+    </div>
+  )
+}
+
+
+const mapStateToProps = state => ({
+  counter: state.counter
+});
+const mapDispachToProp = dispatch => ({
+  increment: function () {
+    dispatch(incAction());
+  },
+  addNumber: function (num) {
+    dispatch(addAction(num))
+  }
+});
+
+export default connect(mapStateToProps, mapDispachToProp)(Home);
