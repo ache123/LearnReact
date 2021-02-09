9ecdf9f6ff31f873d4ef5398384564583f842613
ache123 <631346398@qq.com>
2021-02-09 23:28:40 +0800

    redux-devtools配置

--- 14_learn_react_redux/src/store/index.js
+++ 14_learn_react_redux/src/store/index.js
@@ -1,13 +1,22 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 
 import thunkMiddleware from 'redux-thunk'
 
 import reducer from './reducer.js';
 
 // 应用一些中间件
-const storeenhance = applyMiddleware(thunkMiddleware);
+const storeEnhance = applyMiddleware(thunkMiddleware);
 
-const store = createStore(reducer, storeenhance);
+
+
+// 使用Redux DevTools写法
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({trace: true}) || compose;
+const store = createStore(reducer, composeEnhancers(storeEnhance));
+
+
+// 正常写法
+// const store = createStore(reducer,storeEnhance);
+
 
 
 export default store;
