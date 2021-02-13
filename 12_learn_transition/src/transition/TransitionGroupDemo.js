456b626abb41435d402da4092bea58f3509e8fc8
ache123 <631346398@qq.com>
2021-02-13 11:49:36 +0800

    补充

--- 12_learn_transition/src/transition/TransitionGroupDemo.js
+++ 12_learn_transition/src/transition/TransitionGroupDemo.js
@@ -11,24 +11,25 @@ export default class TransitionGroupDemo extends PureComponent {
   }
   render() {
     return (
-
-      <TransitionGroup>
-        {
-          this.state.names.map((item, index) => {
-            return (
-              <CSSTransition key={item}
-                             timeout={500}
-                             classNames="item">
-                <div>
-                  {item}
-                  <button onClick={e => this.removeItem(index)}>-</button>
-                </div>
-              </CSSTransition>
-            )
-          })
-        }
+      <div>
+        <TransitionGroup>
+          {
+            this.state.names.map((item, index) => {
+              return (
+                <CSSTransition key={item}
+                  timeout={500}
+                  classNames="item">
+                  <div>
+                    {item}
+                    <button onClick={e => this.removeItem(index)}>-</button>
+                  </div>
+                </CSSTransition>
+              )
+            })
+          }
+        </TransitionGroup>
         <button onClick={e => this.addName()}>添加</button>
-      </TransitionGroup>
+      </div>
 
 
     )
@@ -36,7 +37,7 @@ export default class TransitionGroupDemo extends PureComponent {
 
   removeItem(index) {
     this.setState({
-      names: this.state.names.filter((item,indey) => indey !== index)
+      names: this.state.names.filter((item, indey) => indey !== index)
     })
   }
 
